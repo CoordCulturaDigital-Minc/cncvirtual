9eca08d5adbb05c18ea8822cde7108cf2b5a1316
Fernão Lopes <fernao@riseup.net>
2013-11-14 18:52:57 -0200

    listagem do objeto inteiro

--- parts/loop-single-list-title-taxonomy.php
+++ parts/loop-single-list-title-taxonomy.php
@@ -1,20 +1,14 @@
 <li>
     <div class="interaction clearfix">
-        <h1>
-            <a href="<?php the_permalink();?>" title="<?php the_title_attribute();?>"><?php the_title();?></a>
-            <?php if (get_post_meta(get_the_ID(), '_user_created', true)) : ?>
-                <span class="suggest_author">(sugerido por <?php the_author_posts_link(); ?></a>)</span>
-            <?php endif; ?>
-        </h1>
-        
-        <?php if ($post->post_excerpt): ?>
+        <header>
+            <h1>
+                <a href="<?php the_permalink();?>" title="<?php the_title_attribute();?>"><?php the_title();?></a>
+            </h1>
+		<p><?php the_content(); /* hack para fazer apenas uma pequena alteracao, somente para a CNC Virtual  */ ?></p>
             <div class="clear"></div>
-            <p><?php echo $post->post_excerpt; ?></p>
-        <?php endif; ?>
-        
-        <div class="clear"></div>
-        
-        <a href="<?php the_permalink();?>#comments"><div class="comments-number" title="<?php _e('Quantidade de comentários', 'consulta');?>"><?php comments_number('0','1','%');?></div></a>
+        </header>
+
+        <div class="comments-number" title="<?php comments_number('nenhum comentário','1 comentário','% comentários');?>"><?php comments_number('0','1','%');?></div>
         <?php if (get_theme_option('use_evaluation')) : ?>
             <?php html::part('show_evaluation'); ?>
         <?php endif; ?>
